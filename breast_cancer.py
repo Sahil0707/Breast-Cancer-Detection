661e486d434707105fffc60c131e5b98a74c9d18
Sahil Malhotra <97399171+Sahil0707@users.noreply.github.com>
2025-01-29 12:13:30 +0530

    Update breast_cancer.py

--- breast_cancer.py
+++ breast_cancer.py
@@ -1,148 +1,117 @@
 import pickle
-import streamlit as slt
+import streamlit as st
 from streamlit_option_menu import option_menu
-import tensorrt
-from keras import models
-from keras.engine.functional import Functional
-import tensorflow as tf
 
-
-
-#loading Saved Files
-
-
+# Load the saved model
 breast_model = pickle.load(open('Breast_Cancer.sav', 'rb'))
 
-
-#slider bar
-
-with slt.sidebar:
-    selected = option_menu('Breast Cancer Prediction',
-                           ['Breast Cancer'],
-                           icons=['person'],
+# Sidebar menu
+with st.sidebar:
+    selected = option_menu('Breast Cancer Prediction', 
+                           ['Breast Cancer Prediction'], 
+                           icons=['person'], 
                            default_index=0)
 
-
-#Breast Cancer Page:
-
+# Breast Cancer Prediction Page
 if selected == "Breast Cancer Prediction":
-    #page title
+    st.title("Breast Cancer Prediction Using ML")
 
-    slt.title("Breast Cancer Prediction Using Ml")
-
-    # getting the input data from the user
-    col1, col2, col3, col4, col5 = slt.columns(5)
+    # Getting input data from the user
+    col1, col2, col3, col4, col5 = st.columns(5)
 
     with col1:
-        Mean_Radius = slt.text_input('mean radius ')
-
+        Mean_Radius = st.text_input('Mean Radius')
     with col2:
-        Mean_Texture = slt.text_input('mean texture')
-
+        Mean_Texture = st.text_input('Mean Texture')
     with col3:
-        Mean_Perimeter = slt.text_input('mean perimeter')
-
+        Mean_Perimeter = st.text_input('Mean Perimeter')
     with col4:
-        Mean_Area = slt.text_input('mean area')
-
+        Mean_Area = st.text_input('Mean Area')
     with col5:
-        Mean_Smoothness = slt.text_input('mean smoothness')
+        Mean_Smoothness = st.text_input('Mean Smoothness')
 
     with col1:
-        Mean_Compactness = slt.text_input('mean compactness')
-
+        Mean_Compactness = st.text_input('Mean Compactness')
     with col2:
-        Mean_Concavity = slt.text_input('mean concavity')
-
+        Mean_Concavity = st.text_input('Mean Concavity')
     with col3:
-        Mean_Concave_Points = slt.text_input('mean concave points ')
-
+        Mean_Concave_Points = st.text_input('Mean Concave Points')
     with col4:
-        Mean_Symmetry = slt.text_input('mean symmetry')
-
+        Mean_Symmetry = st.text_input('Mean Symmetry')
     with col5:
-        Mean_Fractional_Dimension = slt.text_input('mean fractal dimension')
+        Mean_Fractal_Dimension = st.text_input('Mean Fractal Dimension')
 
     with col1:
-        Radius_Error = slt.text_input('radius error')
-
+        Radius_Error = st.text_input('Radius Error')
     with col2:
-        Texture_Error = slt.text_input('texture error')
-
+        Texture_Error = st.text_input('Texture Error')
     with col3:
-        Perimeter_Error = slt.text_input('perimeter error')
-
-    with col3:
-        Area_Error = slt.text_input('area error')
-
+        Perimeter_Error = st.text_input('Perimeter Error')
     with col4:
-        Smoothness_Error = slt.text_input('smoothness error')
-
+        Area_Error = st.text_input('Area Error')
     with col5:
-        Compactness_Error = slt.text_input('compactness error')
+        Smoothness_Error = st.text_input('Smoothness Error')
 
     with col1:
-        Concavity_Error = slt.text_input('concavity error')
-
+        Compactness_Error = st.text_input('Compactness Error')
     with col2:
-        Concave_Point_Error = slt.text_input('concave point error')
-
+        Concavity_Error = st.text_input('Concavity Error')
     with col3:
-        Symmetry_Error = slt.text_input('symmetry error')
-
+        Concave_Point_Error = st.text_input('Concave Point Error')
     with col4:
-        Fractal_Dimension_Error = slt.text_input('fractal dimension error')
-
+        Symmetry_Error = st.text_input('Symmetry Error')
     with col5:
-        Worst_Radius = slt.text_input('worst radius')
-
+        Fractal_Dimension_Error = st.text_input('Fractal Dimension Error')
 
     with col1:
-        Worst_Texture = slt.text_input('worst texture')
-
+        Worst_Radius = st.text_input('Worst Radius')
     with col2:
-        Worst_Perimeter = slt.text_input('worst perimeter')
-
+        Worst_Texture = st.text_input('Worst Texture')
     with col3:
-        Worst_Area = slt.text_input('worst area')
-
+        Worst_Perimeter = st.text_input('Worst Perimeter')
     with col4:
-        Worst_Smoothness = slt.text_input('worst smoothness')
-
+        Worst_Area = st.text_input('Worst Area')
     with col5:
-        Worst_Compactness = slt.text_input('worst compactness')
+        Worst_Smoothness = st.text_input('Worst Smoothness')
 
     with col1:
-        Worst_Concavity = slt.text_input('worst concavity')
-
+        Worst_Compactness = st.text_input('Worst Compactness')
     with col2:
-        Worst_Concave_Points = slt.text_input('worst concave points')
-
+        Worst_Concavity = st.text_input('Worst Concavity')
     with col3:
-        Worst_Symmetry = slt.text_input('worst symmetry')
-
+        Worst_Concave_Points = st.text_input('Worst Concave Points')
     with col4:
-        Worst_Fractal_Dimension = slt.text_input('worst fractal dimension')
-
+        Worst_Symmetry = st.text_input('Worst Symmetry')
     with col5:
-        Label = slt.text_input('label')
+        Worst_Fractal_Dimension = st.text_input('Worst Fractal Dimension')
 
+    # Convert input to float and handle empty fields
+    try:
+        features = [float(x) if x else 0 for x in [
+            Mean_Radius, Mean_Texture, Mean_Perimeter, Mean_Area, Mean_Smoothness,
+            Mean_Compactness, Mean_Concavity, Mean_Concave_Points, Mean_Fractal_Dimension,
+            Radius_Error, Texture_Error, Perimeter_Error, Area_Error, Smoothness_Error,
+            Compactness_Error, Concavity_Error, Concave_Point_Error, Symmetry_Error,
+            Fractal_Dimension_Error, Worst_Radius, Worst_Texture, Worst_Perimeter,
+            Worst_Area, Worst_Smoothness, Worst_Compactness, Worst_Concavity,
+            Worst_Concave_Points, Worst_Symmetry, Worst_Fractal_Dimension
+        ]]
+    except ValueError:
+        st.warning("Please enter valid numeric values.")
 
-    #code for BC Prediction
+    # Code for Breast Cancer Prediction
+    breast_cancer_diagnosis = ""
 
-    breast_cancer_diagnosis = ''
+    if st.button('Breast Cancer Test Result'):
+        prediction = breast_model.predict([features])
 
-
-    #Breast Cancer Prediction Page
-
-
-    if slt.button('Breast Cancer Test Result'):
-        breast_cancer_prediction = breast_model.predict([[Mean_Radius, Mean_Texture, Mean_Perimeter, Mean_Area, Mean_Smoothness, Mean_Compactness, Mean_Concavity, Mean_Concave_Points, Mean_Fractional_Dimension, Radius_Error, Texture_Error, Perimeter_Error, Area_Error, Smoothness_Error, Compactness_Error, Concavity_Error, Concave_Point_Error, Symmetry_Error, Fractal_Dimension_Error, Worst_Radius, Worst_Texture, Worst_Perimeter, Worst_Area, Worst_Smoothness, Worst_Compactness, Worst_Concavity, Worst_Concave_Points, Worst_Symmetry, Worst_Fractal_Dimension, Label]])
-
-        if breast_cancer_prediction[0] == 1:
-            """The Person has Breast Cancer"""
-
+        if prediction[0] == 1:
+            breast_cancer_diagnosis = "The person has Breast Cancer."
         else:
+            breast_cancer_diagnosis = "The person is Cancer-Free."
+
+        st.success(breast_cancer_diagnosis)
+
             """The Person is Cancer Free"""
 
-    slt.success(breast_cancer_diagnosis)
+    st.success(breast_cancer_diagnosis)
